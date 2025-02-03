27b342d8d933f99b0245952a8880f88b735c8c8e
rahulapps29 <rahulapps29@gmail.com>
2025-02-04 01:02:21 +0530

    added credentials to the .env file

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,14 +4,19 @@ require('dotenv').config();
 
 const router = express.Router();
 
-const ADMIN_USERNAME = 'admin';
-const ADMIN_PASSWORD = 'password';  // You can change this as needed
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
-    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '2h' });
+    const token = jwt.sign(
+      { username },
+      process.env.JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN }
+    );
     return res.json({ token });
   }
 
--- utils/linkGenerator.js
+++ utils/linkGenerator.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
 const { v4: uuidv4 } = require('uuid');
 
 const generateUniqueLink = (identifier, type = 'email') => {
   const token = jwt.sign(
     { identifier, type, nonce: uuidv4() },  // Adding unique nonce
     process.env.JWT_SECRET,
-    { expiresIn: '1h' }
+    { expiresIn: process.env.JWT_EXPIRES_IN }
   );
   return `https://flipkart.algoapp.in/form?token=${token}`;
 };
